ab15069bad4d6c56ebeabf92413ed17250e1a33c
Utkarsh dubey <125132498+UtkarshDubey01@users.noreply.github.com>
2023-11-01 22:03:06 +0530

    Add files via upload

---
+++ My new task/script.js
@@ -0,0 +1,36 @@
+var image = document.getElementById("pfimage");
+var info = document.getElementById("subject");
+
+function btn1(){  
+    image.src="images/tch1.jpg";
+    info.innerHTML="MATHEMATICS TEACHER";
+}
+
+function btn2(){  
+    image.src="images/tch2.jpg";
+    info.innerHTML="PHYSICS TEACHER";
+}
+function btn3(){  
+    image.src="images/tch3.jpeg";
+    info.innerHTML="CHEMISTRY TEACHER";
+}
+function btn4(){  
+    image.src="images/tch4.jpeg";
+    info.innerHTML="BIOLOGY TEACHER";
+}
+function btn5(){  
+    image.src="images/tch5.png";
+    info.innerHTML="COMPUTER TEACHER";
+}
+function btn6(){  
+    image.src="images/tch6.jpg";
+    info.innerHTML="ENGLISH TEACHER";
+}
+function btn7(){  
+    image.src="images/tch7.jpg";
+    info.innerHTML="GEOGRAPHY TEACHER";
+}
+function btn8(){  
+    image.src="images/tch8.jpg";
+    info.innerHTML="HINDI TEACHER";
+}
